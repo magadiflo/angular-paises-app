a1aedb1da45ecc1e15dc29eed61d0e99edabef9c
Martín <magadiflo@gmail.com>
2022-07-18 23:57:00 -0500

    Creando los módulos y los componentes básicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { SharedModule } from './shared/shared.module';
+import { PaisModule } from './pais/pais.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    PaisModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
