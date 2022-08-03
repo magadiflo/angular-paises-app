0bc49e144933c695f42470912219c6534bf9ecf4
Martín <magadiflo@gmail.com>
2022-08-02 22:09:15 -0500

    Terminar la pantalla de ver país

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
 
 import { PaisService } from '../../services/pais.service';
 
@@ -12,6 +13,8 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private paisService: PaisService) { }
@@ -19,11 +22,10 @@ export class VerPaisComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.params
       .pipe(
-        switchMap(({ id }) => this.paisService.getPaisPorAlpha(id)) //* switchMap, recibe el valor del observable anterior y retorna un nuevo observable
+        switchMap(({ id }) => this.paisService.getPaisPorAlpha(id)), //* switchMap, recibe el valor del observable anterior y retorna un nuevo observable
+        tap(pais => console.log(pais)) //* Operador que dispara un efecto secundario. Solo lo usaremos para mostrar el resultado que devuelve el observable del switchMap
       )
-      .subscribe(pais => {
-        console.log(pais);
-      });
+      .subscribe(pais => this.pais = pais);
   }
 
 }
